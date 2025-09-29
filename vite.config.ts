52b71176f13b81ebe8e8b7e7a03ca69dec3567d8
KarthikeyaThotak <karthikeyathotak@gmail.com>
2025-09-29 01:12:28 +0000

    Added domain to vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
     port: 8080,
+    allowedHosts:[
+	    'lab.karthikeyathota.page',
+    ],
+
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
